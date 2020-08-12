9d1ff207550dddcd56909e37af36d9fd02fcb1e1
louis <louis_ouellet@hotmail.com>
2020-08-12 10:28:28 -0400

    UPDATE

--- example2.php
+++ example2.php
@@ -3,7 +3,7 @@
 // We need to include the LSP Class
 require_once('lsp.php');
 
-// Checks are done by verifying if the server replied and validating it's replied against the hash.
+// Checks are done by verifying if the server replied and validating it's reply against the hash.
 // In this case a variable $token->Status will be used to display the application or display an activation form instead.
 $token = new LSP('http://localhost/index2.php','12345-12345-12345-12345-12345','$2y$10$BnwjifGipuexhkZGoEiIO.3ogtar42OyU/CSYkORpSV69OySS9is2',TRUE);
 
