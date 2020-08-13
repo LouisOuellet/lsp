97fa1f1ca458d6573a096e2e8a3efc47632c8706
louis <louis_ouellet@hotmail.com>
2020-08-13 13:57:32 -0400

    UPDATE

--- example.php
+++ example.php
@@ -5,7 +5,7 @@ require_once('lsp.php');
 
 // Checks are done by verifying if the server replied and validating it's reply against the hash.
 // If it doesn't, then the class will exit the code and display Invalid License
-$token = new LSP('http://localhost/','example','a76e-241c-242f-4b67-208f-0d23-1f37-da1a','$2y$10$CUNaEQPuRhHG1v3TwzLzQ.VJZEWL73C8p45BGfmmKvCNTHzSubHea');
+$token = new LSP('http://localhost/','Example','afa1-f539-31bb-f980-ad8a-ec49-a4f3-85c3','$2y$10$IRrnap4vZtxZTXWDA/itXuxoutVnmjboRypgIhReHPAqGrMWqK9V6');
 
 // You can start your application now
 echo 'Start Application';
