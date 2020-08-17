feca8f80a676cdda058ec1895c80ab565748da0c
louis <louis_ouellet@hotmail.com>
2020-08-17 11:19:51 -0400

    UPDATE

--- example.php
+++ example.php
@@ -4,10 +4,20 @@
 require_once('lsp.php');
 
 // Checks are done by verifying if the server replied and validating it's reply against the hash.
-$lsp = new LSP('http://localhost/','Example','3128-54c3-2317-2afd-a3fd-6790-4f2b-48f2','$2y$10$pe1JZLe8S0Mu8GNTHwf9rOPX/zJwb1DfUemKjz5G9oGVypHFfnFim');
+$lsp = new LSP('host','application','key','token');
 
 // In this case a variable $lsp->Status will be used to display the application or display an activation form instead.
 if($lsp->Status){
+	if($lsp->Update){
+		// We configure our database access
+		$lsp->configdb('host', 'username', 'password', 'example');
+		// We update the local files
+		$lsp->update('branch');
+		// We start updating our database
+		$lsp->updatedb('db.json');
+	} else {
+		echo 'No Updates Available';
+	}
 
 	// You can start your application now
 	echo 'Start Application';
